5543998ad54132b05caa02b00dbc245d1490b267
Abhishek Kumar <69464339+abhikashyapr22@users.noreply.github.com>
2022-11-07 17:57:05 +0530

    Update and rename How to remove outliers (three methods) to How to remove outliers (three methods).py

---
+++ How to remove outliers (three methods).py
@@ -0,0 +1,67 @@
+# 1. Standard deviation method
+
+# identify outliers with standard deviation
+from numpy.random import seed
+from numpy.random import randn
+from numpy import mean
+from numpy import std
+# seed the random number generator
+seed(1)
+# generate univariate observations
+data = 5 * randn(10000) + 50
+# calculate summary statistics
+data_mean, data_std = mean(data), std(data)
+# identify outliers
+cut_off = data_std * 3
+lower, upper = data_mean - cut_off, data_mean + cut_off
+# identify outliers
+outliers = [x for x in data if x < lower or x > upper]
+print('Identified outliers: %d' % len(outliers))
+# remove outliers
+outliers_removed = [x for x in data if x >= lower and x <= upper]
+print('Non-outlier observations: %d' % len(outliers_removed))
+
+
+# 2. Interquartile range method
+
+# identify outliers with interquartile range
+from numpy.random import seed
+from numpy.random import randn
+from numpy import percentile
+# seed the random number generator
+seed(1)
+# generate univariate observations
+data = 5 * randn(10000) + 50
+# calculate interquartile range
+q25, q75 = percentile(data, 25), percentile(data, 75)
+iqr = q75 - q25
+print('Percentiles: 25th=%.3f, 75th=%.3f, IQR=%.3f' % (q25, q75, iqr))
+# calculate the outlier cutoff
+cut_off = iqr * 1.5
+lower, upper = q25 - cut_off, q75 + cut_off
+# identify outliers
+outliers = [x for x in data if x < lower or x > upper]
+print('Identified outliers: %d' % len(outliers))
+# remove outliers
+outliers_removed = [x for x in data if x >= lower and x <= upper]
+print('Non-outlier observations: %d' % len(outliers_removed))
+
+
+# 3. Automatic outlier detection
+
+# load and summarize the dataset
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+# load the dataset
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/housing.csv'
+df = read_csv(url, header=None)
+# retrieve the array
+data = df.values
+# split into inpiut and output elements
+X, y = data[:, :-1], data[:, -1]
+# summarize the shape of the dataset
+print(X.shape, y.shape)
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+# summarize the shape of the train and test sets
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
